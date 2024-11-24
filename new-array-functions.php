ed79b6cd904489b7afbb0584c9d5f2d4f0bf3d87
s-damian <contact@damian-freelance.fr>
2024-11-24 11:01:02 +0300

    New Array Functions

--- new-array-functions.php
+++ new-array-functions.php
@@ -11,22 +11,22 @@ $arrayD = [111, 222, 'bbb', 444, 555];
 
 /*
 |--------------------------------------------------------------------------
-| array_find:
+| array_find
 | Returns the value of the first element from the array for which the callback returns true.
 |--------------------------------------------------------------------------
 */
 
-$test = array_find($arrayA, fn($value) => mb_strlen($value) === 5);
+$test = array_find($arrayA, fn($value, $key) => mb_strlen($value) === 5);
 var_dump( $test );
 // var_dump result: string(5) "bbbbb"
 
-$test = array_find($arrayB, fn($value) => mb_strlen($value) === 5);
+$test = array_find($arrayB, fn($value, $key) => mb_strlen($value) === 5);
 var_dump( $test );
 // var_dump result: string(5) "bbbbb"
 
 /*
 |--------------------------------------------------------------------------
-| array_find_key:
+| array_find_key
 | Returns the key of the first element from the array for which the callback returns true.
 |--------------------------------------------------------------------------
 */
@@ -40,7 +40,7 @@ var_dump( $test ); // string(1) "b"
 
 /*
 |--------------------------------------------------------------------------
-| array_all:
+| array_all
 | Takes an array and a callback, and returns true if all of the elements in the array return true when passed to the callback.
 |--------------------------------------------------------------------------
 */
@@ -50,4 +50,11 @@ var_dump( $test ); // bool(true)
 
 $test = array_all($arrayD, fn($value, $key) => is_numeric($value));
 var_dump( $test ); // bool(false)
+
+/*
+|--------------------------------------------------------------------------
+| array_any
+| Takes an array and a callback, and returns true if all of the elements in the array return true when passed to the callback.
+|--------------------------------------------------------------------------
+*/
 
